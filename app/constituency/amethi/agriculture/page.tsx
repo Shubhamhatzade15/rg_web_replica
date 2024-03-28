574c944b64b8351f49aa21930577e85dc2e29b60
Asad <asad200ana@gmail.com>
2024-03-29 04:52:00 +0530

    amethi/agriculture page completed

--- app/constituency/amethi/agriculture/page.tsx
+++ app/constituency/amethi/agriculture/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Hero from "./hero";
+import Content from "./content";
 
 function page() {
   return (
     <div>
       <Hero />
+      <Content />
     </div>
   );
 }
