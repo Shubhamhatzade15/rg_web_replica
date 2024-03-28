d9aa91401733e56fea1e53eca06b8bdd9aec6842
ShreejaMehta <shreejamehta1609@gmail.com>
2024-03-28 12:32:06 +0530

    🐛 Fix: Added breakpoints to make about page responsive

---
+++ app/about/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div>
+        <Image
+        src="/assets/Rahul.jpg"
+        alt="Vercel Logo"
+        width={1980}
+        height={1223}
+      />
+    </div>
+  )
+}
+
+export default Hero
