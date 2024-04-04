12efe838fbaa125dbb6a6cd0446e65b4ca69a5a2
Kalpak Goshikwar <kalpakgoshikwar123@gmail.com>
2024-04-04 19:39:08 +0530

    ADD: constituency subpages

---
+++ app/constituency/bhandara/page.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../../issues/style.css";
+
+export default function bhandara() {
+  const issues = [
+    {
+      title: "Tumsar",
+      link: "/political-vision/job-creation-and-msmes",
+    },
+    {
+      title: "Bhandara (SC)",
+      link: "/political-vision/agriculture",
+    },
+    {
+      title: "Sakoli",
+      link: "/political-vision/land-acquisition-bill",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="p-4 px-6 lg:px-60 md:px-20 text-[#337ab7]">
+        {issues.map((issue, index) => {
+          return (
+            // <div
+            //   key={index + 1}
+            //   className="uppercase block py-4 px-2 border-b-2 border-[#337ab7] w-full group relative cursor-pointer"
+            // >
+            //   <a
+            //     href={issue.link}
+            //     className="block w-full h-full group-hover:text-white transition-all duration-800 uppercase"
+            //   >
+            //     {issue.title}
+            //   </a>
+            //   <div className="h-0 w-full opacity-0 group-hover:h-full group-hover:opacity-100 bg-[#337ab7] absolute top-1/2 group-hover:top-0 left-0 transition-all duration-800 z-[-1]"></div>
+            // </div>
+            <div
+              key={index + 1}
+              className="hvr-shutter-out-vertical uppercase block border-b border-[#337ab7] w-full group relative cursor-pointer"
+            >
+              <a
+                href={issue.link}
+                className="block w-full h-full py-4 px-2 group-hover:text-white"
+              >
+                {issue.title}
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- app/constituency/states.tsx
+++ app/constituency/states.tsx
@@ -14,7 +14,7 @@ const states = () => {
   return (
     <main className={mons.className}>
       <div className="w-full h-full flex justify-center items-center mt-10 px-10">
-        <div className="grid grid-cols-1 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
           <div className="bg-[#B7D1E2] p-5 flex flex-col justify-center items-center">
             <Image
               src="/constituency/bhandara_gondia.jpg"
@@ -25,10 +25,26 @@ const states = () => {
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
             <Link
-              href="https://en.wikipedia.org/wiki/Bhandara%E2%80%93Gondiya_Lok_Sabha_constituency"
+              href="/constituency/bhandara"
               className="text-[16px] md:text-[20px] lg:text-[25px] tracking-tighter underline mt-10 hover:text-gray-500 "
             >
-              Bhandara - Gondiya
+              Bhandara
+            </Link>
+          </div>
+          <div className="bg-[#B7D1E2] p-5 flex flex-col justify-center items-center">
+            <Image
+              src="/constituency/bhandara_gondia.jpg"
+              quality={100}
+              width={300}
+              height={300}
+              alt="Rahul Gandhi in Wayanad"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+            <Link
+              href="/constituency/gondia"
+              className="text-[16px] md:text-[20px] lg:text-[25px] tracking-tighter underline mt-10 hover:text-gray-500 "
+            >
+              Gondia
             </Link>
           </div>
         </div>
